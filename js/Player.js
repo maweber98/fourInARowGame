55b2be3d6f70b118bb31f8f6fd1dcd00a7891631
maweber98 <weberdesignstudio@gmail.com>
2020-09-15 14:31:33 -0400

    new four in a row game

---
+++ js/Player.js
@@ -0,0 +1,47 @@
+class Player {
+    constructor(name, id, color, active = false) {
+        this.name = name;
+        this.id = id;
+        this.color = color;
+        this.active = active;
+        this.tokens = this.createTokens(21);
+    }
+   /**
+    * Creates token object for player
+    * @param {integer} num - Number of token objects to be created
+    * @return {array} tokens - an array of new token objects 
+    */
+    createTokens(num) {
+        const tokens = [];
+        
+        for(let i = 0; i < num; i++) {
+            let token = new Token(i, this);
+            tokens.push(token);
+        }
+
+        return tokens;
+    }
+    /**
+     * Gets all of the tokens that haven't been dropped
+     * @return {array} Array of unused tokens
+     */
+    get unusedTokens() {
+        return this.tokens.filter(token => !token.dropped);
+    }
+
+    /**
+     * Gets active token by returning the first token in the array of unused tokens
+     * @return {object} First token object in the array of unused tokens
+     */
+    get activeToken() {
+        return this.unusedTokens[0];
+    }
+
+    /**
+     * Check if a player has any undropped tokens left
+     * @return {Boolean} 
+     */
+    checkTokens() {
+        return this.unusedTokens.length == 0 ? false : true;
+    }
+}
---
+++ js/app.js
@@ -0,0 +1,13 @@
+const game = new Game();
+
+document.getElementById('begin-game').addEventListener('click', function(){
+    game.startGame();
+
+    this.style.display = 'none';
+    document.getElementById('play-area').style.opacity = '1';
+});
+
+document.addEventListener('keydown', function(event){
+    // console.log(event.key);
+    game.handleKeydown(event);
+});
